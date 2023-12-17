87ab26b8a4a6725d1b835a134e9a6d328c2c38d3
Nhat Tran <minhnhat.17ck1@gmail.com>
2023-12-17 13:49:18 -0600

    Taskly - Tasks Management

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import { useOrganizationList } from '@clerk/nextjs';
+
+export const OrgControl = () => {
+  return null;
+};
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "Taskly",
+    description: "Collaborative task management. Create, assign, and track tasks with ease.",
+}
