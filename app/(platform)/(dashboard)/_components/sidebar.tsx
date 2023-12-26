a12587178c274996be6f6815cd9cd666b63a6af1
Nhat Tran <minhnhat.17ck1@gmail.com>
2023-12-26 14:06:03 -0600

    Sidebar for Desktop
    Next will be sidebar for mobile

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -10,6 +10,8 @@ import { Separator } from '@/components/ui/separator';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Accordion } from '@/components/ui/accordion';
 
+import { NavItem, Organization } from './nav-item';
+
 interface SidebarProps {
   storageKey?: string;
 }
@@ -69,6 +71,21 @@ export const Sidebar = ({ storageKey = 't-sidebar-state' }: SidebarProps) => {
           </Link>
         </Button>
       </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
+      </Accordion>
     </>
   );
 };
